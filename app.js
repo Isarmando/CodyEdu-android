a85c4d4a65dabaa0cad2a59ff5af3f8651633397
Momokameo <72054645+Momokameo@users.noreply.github.com>
2021-12-04 13:53:30 -0600

    .

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-
+console.log('Se inicio el engine');
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({extended:false}));
 app.use(express(json));
 
 app.use('/', require('./router'));
-
-app.set('port', process.env.PORT||5000);
+console.log('Se inicio el router');
+app.set('port', process.env.PORT||3000);
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
